de677d687833288dc87e704ab3ed82f2ff29315c
Ignasiya <ignasiya93@gmail.com>
2024-04-17 22:05:22 +0300

    семинар 11

--- arhive-lesson/seminar-11/data.js
+++ arhive-lesson/seminar-11/data.js
@@ -1,7 +1,6 @@
 const dataProducts = `[
     {
         "id": 1,
-        "class": "product",
         "name": "MANGO PEOPLE T-SHIRT",
         "cover": "Rectangle-1.jpeg",
         "price": 300,
@@ -11,7 +10,6 @@ const dataProducts = `[
     },
     {
         "id": 2,
-        "class": "product",
         "name": "ELLERY X M'O CAPSULE",
         "cover": "Rectangle-2.jpeg",
         "price": 52,
